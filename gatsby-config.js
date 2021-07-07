841bf8eb2225982fcdb2645141fcc423ec4915e8
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-07-07 12:48:27 +1200

    updated data

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `John Aupouri's Portfolio`,
+        short_name: `Johns Portfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
--- src/mock/data.js
+++ src/mock/data.js
@@ -10,7 +10,7 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
+  name: 'John',
   subtitle: '',
   cta: '',
 };
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'about.png',
-  paragraphOne: '',
+  paragraphOne: 'My name is John Wirihana Aupouri and I am a full-stack web developer.',
   paragraphTwo: '',
   paragraphThree: '',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
@@ -30,28 +30,29 @@ export const projectsData = [
     id: nanoid(),
     img: 'kiwi-roadtrip.png',
     title: 'Kiwi Roadtrip',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    info: 'A roadtrip app designed to give users the kiwi experience.',
+    info2: 'This app was a collaborative effort.',
+    url: 'https://kiwiroadtrip.herokuapp.com/',
+    repo: 'https://github.com/kotare-2019/roadtrip', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'fakify-live.png',
     title: 'Crown Clothing',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    info: 'An e-commerce app built using React for the UI, Redux Sagas for state management, and Firebase to handle user authentication and data storage.',
+    info2:
+      'I am using styled-components and the Stripe API will take care of mock payments made via an Express server also am using react lazy to break code into chunks for performance optimization.',
+    url: 'https://fakify-live.herokuapp.com/',
+    repo: 'https://github.com/john-w-aupouri/fakify-live', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'devPlace.png',
     title: 'DevPlace',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    info: 'A social media app built using the MERN-Stack.',
+    info2: 'A place for developers.',
+    url: 'https://dev-place.herokuapp.com/',
+    repo: 'https://github.com/john-w-aupouri/devPlace', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +60,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'johnaupouri963@gmail.com',
 };
 
 // FOOTER DATA
@@ -80,5 +81,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
